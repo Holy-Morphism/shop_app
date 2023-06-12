23e7cfbde878c31092bc7f5c9ef841e177e90b83
Holy-Morphism <ranaadeeltahir@gmail.com>
2023-06-13 00:39:51 +0500

    base file created

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('MyShop'),
+      ),
+      body: Center(
+        child: Text('Let\'s build a shop!'),
+      ),
+    );
+  }
+}
